66b721c834be6b643576e8794b02300274236cb2
Fluff <112392081+Fluff74@users.noreply.github.com>
2025-12-19 20:29:54 -0500

    Seal is basically finished until the spritesheet is done.

--- SealSoccer/Seal.cs
+++ SealSoccer/Seal.cs
@@ -26,16 +26,22 @@ namespace SealSoccer
         /// </summary>
         public int Speed { get; set; }
 
-        Texture2D galumphSpritesheet; // Used while the seal is moving.
-        Rectangle hitbox; // This is where the seal is drawn from.
+        Texture2D spritesheet; // Used while the seal is moving.
+        readonly Vector2 origin; // The origin we're drawing the seal from.
+        Rectangle drawbox; // This is where the seal is drawn from.
+        Rectangle hitbox; // This is the seal's hitbox, where it can hit the ball from.
+        Rectangle source; // The source rectangle on the spritesheet that we're drawing the seal from.
 
-        public Seal(Texture2D galumphSpritesheet)
+        public Seal(Texture2D spritesheet)
         {
             facing = Facing.Right; // Start the seal facing to the right.
-            Speed = 5;
+            Speed = 20; // Always between 20 and 30.
 
-            this.galumphSpritesheet = galumphSpritesheet;
-            hitbox = new Rectangle(1645, 1750, 550, 320);
+            this.spritesheet = spritesheet;
+            origin = new(0.0f, 0.0f);
+            drawbox = new(1645, 1750, 550, 320);
+            hitbox = new(1645, 1790, 550, 280);
+            source = new(0, 0, 183, 107);
         }
 
         public void Update(GameTime gameTime)
@@ -52,7 +58,13 @@ namespace SealSoccer
             // Move the seal to the right.
             if(rightward)
             {
-                hitbox.X += Speed;
+                drawbox.X += Speed;
+
+                // If the seal leaves the right bound of the screen, stop it from moving further.
+                if(drawbox.X > 3290)
+                {
+                    drawbox.X = 3290;
+                }
 
                 // Make the seal face right, if it is currently facing left.
                 if(facing == Facing.Left)
@@ -64,7 +76,13 @@ namespace SealSoccer
             // Move the seal to the left.
             else
             {
-                hitbox.X -= Speed;
+                drawbox.X -= Speed;
+
+                // If the seal leaves the left bound of the screen, stop it from moving.
+                if(drawbox.X < 0)
+                {
+                    drawbox.X = 0;
+                }
 
                 // Make the seal face left, if it is currently facing right.
                 if(facing == Facing.Right)
@@ -72,18 +90,39 @@ namespace SealSoccer
                     facing = Facing.Left;
                 }
             }
+
+            // Update the hitbox's coordinates to match the drawbox's coordinates.
+            hitbox.X = drawbox.X;
         }
 
+        /// <summary>
+        /// Draws the seal onto the screen, facing the proper direction.
+        /// </summary>
+        /// <param name="sb"> The spritebatch we're using to draw the seal. </param>
         public void Draw(SpriteBatch sb)
         {
-            sb.Draw(galumphSpritesheet, hitbox, Color.White);
+            switch (facing)
+            {
+                case Facing.Left:
+
+                    sb.Draw(spritesheet, drawbox, source, Color.White, 0.0f, origin, SpriteEffects.FlipHorizontally, 0.0f);
+
+                    break;
+
+                case Facing.Right:
+
+                    sb.Draw(spritesheet, drawbox, source, Color.White, 0.0f, origin, SpriteEffects.None, 0.0f);
+
+                    break;
+            }
         }
 
         public void Reset()
         {
-            hitbox.X = 1645;
-            hitbox.Y = 1750;
+            drawbox.X = 1645;
+            drawbox.Y = 1750;
             facing = Facing.Right;
+            Speed = 20;
         }
     }
 }
